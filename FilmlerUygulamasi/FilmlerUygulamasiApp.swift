3b5d848b1a1a828a8bf4db70a35b315051ab24c1
HalitAyhanAydin <halitayhanaydin@icloud.com>
2025-03-07 10:56:11 +0300

    Initial Commit

---
+++ FilmlerUygulamasi/FilmlerUygulamasiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmlerUygulamasiApp.swift
+//  FilmlerUygulamasi
+//
+//  Created by Halit Ayhan Aydın on 7.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct FilmlerUygulamasiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
